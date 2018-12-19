d47d573acc7882385ece482b9ca0c03a2e7b86f1
Takaya Ogawa <bumpofchicken344@gmail.com>
2018-12-20 04:40:24 +0900

    clean code

--- Assets/Scripts/InvisibleProcessor.cs
+++ Assets/Scripts/InvisibleProcessor.cs
@@ -10,18 +10,22 @@ public class InvisibleProcessor
     static readonly Scalar SKIN_LOWER = new Scalar(0, 0, 50);
     static readonly Scalar SKIN_UPPER = new Scalar(25, 173, 255);
     static readonly Scalar HAIR_LOWER = new Scalar(0, 0, 0);
-    static readonly Scalar HAIR_UPPER = new Scalar(255, 255, 70);
+    static readonly Scalar HAIR_UPPER = new Scalar(255, 255, 100);
 
     public void SaveBackground(Mat webcamMat)
     {
         _background = webcamMat.clone();
     }
 
+    /// <summary>背景を保存したかどうか</summary>
     public bool HasSavedBackground()
     {
         return _background != null;
     }   
     
+    /// <summary>肌と髪の領域を背景で置換する</summary>
+    /// <param name="webcamMat">カメラからのRGBA画像</param>
+    /// <returns>置換後のRGBA画像</returns>
     public Mat ConvertToInvisible(Mat webcamMat)
     {
         //カメラ映像ををHSVに変換
@@ -31,34 +35,30 @@ public class InvisibleProcessor
         cvtColor(blured, hsv, COLOR_RGBA2RGB);
         cvtColor(hsv, hsv, COLOR_RGB2HSV);
         
-        //肌領域のみ255のマスクを作成
+        //肌と髪の領域と同じ位置の背景領域を抽出
         var skinMask = new Mat();
         inRange(hsv, SKIN_LOWER, SKIN_UPPER, skinMask);
         morphologyEx(skinMask, skinMask, MORPH_OPEN, new Mat(), new Point(-1, -1), 3);
         
-        //髪領域のみ255のマスクを作成成
         var hairMask = new Mat();
         inRange(hsv, HAIR_LOWER, HAIR_UPPER, hairMask);
         morphologyEx(hairMask, hairMask, MORPH_OPEN, new Mat(), new Point(-1, -1), 3);
         
-        //肌と髪のマスクを合成
-        var foreMask = new Mat();
-        bitwise_or(skinMask, hairMask, foreMask);
+        var skinAndHairMask = new Mat();
+        bitwise_or(skinMask, hairMask, skinAndHairMask);
         
-        //肌色と髪の領域を置換する背景マスクを作成
-        var backMask = new Mat();
-        bitwise_and(_background, _background, backMask, foreMask);
+        var bgrOnSkinAndHair = new Mat();
+        bitwise_and(_background, _background, bgrOnSkinAndHair, skinAndHairMask);
         
-        //カメラ映像の肌色領域を黒にする
-        bitwise_not(foreMask, foreMask);
-        var withoutSkin = new Mat();
-        bitwise_and(webcamMat, webcamMat, withoutSkin, foreMask);
+        //肌と髪以外の領域を抽出
+        bitwise_not(skinAndHairMask, skinAndHairMask);
+        var withoutSkinAndHair = new Mat();
+        bitwise_and(webcamMat, webcamMat, withoutSkinAndHair, skinAndHairMask);
         
         //カメラ映像の肌色領域を背景で置換
         var invisible = new Mat();
-        bitwise_or(withoutSkin, backMask, invisible);
+        bitwise_or(withoutSkinAndHair, bgrOnSkinAndHair, invisible);
         
         return invisible;
     }
-    
 }
---
+++ Assets/Scripts/PictureTaker.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PictureTaker : MonoBehaviour 
+{
+	void Start () 
+	{
+		
+	}
+	
+	void Update () 
+	{
+		
+	}
+}

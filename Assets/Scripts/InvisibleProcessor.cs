1c6771d7616bd90de3abedaaf079cd424d729d89
Takaya Ogawa <bumpofchicken344@gmail.com>
2018-12-20 02:27:47 +0900

    convert to invisible on smartphone

---
+++ Assets/Scripts/InvisibleProcessor.cs
@@ -0,0 +1,49 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using OpenCVForUnity;
+using static OpenCVForUnity.Imgproc;
+using static OpenCVForUnity.Core;
+
+public class InvisibleProcessor
+{
+    Mat _background;
+    static readonly Scalar SKIN_LOWER = new Scalar(0, 70, 0);
+    static readonly Scalar SKIN_UPPER = new Scalar(40, 255, 255);
+
+    public void SaveBackground(Mat webcamMat)
+    {
+        _background = webcamMat.clone();
+    }
+    
+    public Mat ConvertToInvisible(Mat webcamMat)
+    {
+        //カメラ映像ををHSVに変換
+        var blured = new Mat();
+        GaussianBlur(webcamMat, blured, new Size(19, 19), 0);
+        var hsv = new Mat();
+        cvtColor(blured, hsv, COLOR_RGBA2RGB);
+        cvtColor(hsv, hsv, COLOR_RGB2HSV);
+        
+        //肌色領域が0のマスクを作成
+        var skinMask = new Mat();
+        inRange(hsv, SKIN_LOWER, SKIN_UPPER, skinMask);
+        morphologyEx(skinMask, skinMask, MORPH_OPEN, new Mat(), new Point(-1, -1), 3);
+//        morphologyEx(skinMask, skinMask, MORPH_CLOSE, new Mat(), new Point(-1, -1), 3);
+        
+        //肌色領域を置換する背景マスクを作成
+        var backMask = new Mat();
+        bitwise_and(_background, _background, backMask, skinMask);
+        
+        //カメラ映像の肌色領域を黒にする
+        bitwise_not(skinMask, skinMask);
+        var withoutSkin = new Mat();
+        bitwise_and(webcamMat, webcamMat, withoutSkin, skinMask);
+        
+        //カメラ映像の肌色領域を背景で置換
+        var invisible = new Mat();
+        bitwise_or(withoutSkin, backMask, invisible);
+        
+        return invisible;
+    }
+    
+}
---
+++ Assets/Scripts/WebCamManager.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+using OpenCVForUnity;
+using OpenCVForUnityExample;
+using UnityEngine;
+using static OpenCVForUnity.Imgproc;
+using static OpenCVForUnity.Core;
+
+[RequireComponent(typeof(WebCamTextureToMatHelper), typeof(FpsMonitor))]
+public class WebCamManager : MonoBehaviour
+{
+    Texture2D _texture;
+    WebCamTextureToMatHelper _webCamTextureToMatHelper;
+    FpsMonitor _fpsMonitor;
+    InvisibleProcessor _invisibleProcessor = new InvisibleProcessor();
+    float time = 5;
+    
+    void Start()
+    {
+        Input.backButtonLeavesApp = true;
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        
+        _fpsMonitor = GetComponent<FpsMonitor>();
+        _webCamTextureToMatHelper = GetComponent<WebCamTextureToMatHelper>();
+        _webCamTextureToMatHelper.Initialize();
+    }
+    
+    void Update()
+    {
+        if (time > 0) {
+            time -= Time.deltaTime;
+            return;
+        }
+        
+        if (!_webCamTextureToMatHelper.IsPlaying() || !_webCamTextureToMatHelper.DidUpdateThisFrame()) return;
+        
+        _invisibleProcessor.SaveBackground(_webCamTextureToMatHelper.GetMat());
+//        var rgbaMat = _webCamTextureToMatHelper.GetMat();
+        var rgbaMat = _invisibleProcessor.ConvertToInvisible(_webCamTextureToMatHelper.GetMat());
+        Utils.fastMatToTexture2D(rgbaMat, _texture);
+    }
+    
+    public void OnWebCamTextureToMatHelperInitialized()
+    {
+        Debug.Log ("OnWebCamTextureToMatHelperInitialized");
+
+        var webCamTextureMat = _webCamTextureToMatHelper.GetMat();
+        _texture = new Texture2D (webCamTextureMat.cols(), webCamTextureMat.rows(), TextureFormat.RGBA32, false);
+        GetComponent<Renderer>().material.mainTexture = _texture;
+
+        Debug.Log ("Screen.width " + Screen.width + " Screen.height " + Screen.height + " Screen.orientation " + Screen.orientation);
+
+        if (_fpsMonitor != null){
+            _fpsMonitor.Add ("width", webCamTextureMat.width().ToString());
+            _fpsMonitor.Add ("height", webCamTextureMat.height().ToString());
+            _fpsMonitor.Add ("orientation", Screen.orientation.ToString());
+        }
+        
+        float width = webCamTextureMat.width();
+        float height = webCamTextureMat.height();
+                                
+        float widthScale = Screen.width / width;
+        float heightScale = Screen.height / height;
+        if (widthScale < heightScale) {
+            Camera.main.orthographicSize = (width * Screen.height / Screen.width) / 2;
+        } else {
+            Camera.main.orthographicSize = height / 2;
+        }
+        
+        //Quadを画面いっぱいにリサイズ
+        ////https: //blog.narumium.net/2016/12/11/unityでスマホカメラを全面表示する/
+        var quadHeight = Camera.main.orthographicSize * 2;
+        var quadWidth = quadHeight * Camera.main.aspect;
+        transform.localScale = new Vector3(quadWidth, quadHeight, 1);
+        
+//        _invisibleProcessor.SaveBackground(_webCamTextureToMatHelper.GetMat());
+    }
+
+    public void OnWebCamTextureToMatHelperDisposed ()
+    {
+        Debug.Log ("OnWebCamTextureToMatHelperDisposed");
+        if (_texture != null) {
+            Destroy(_texture);
+            _texture = null;
+        }
+    }
+    
+    public void OnWebCamTextureToMatHelperErrorOccurred(WebCamTextureToMatHelper.ErrorCode errorCode){
+        Debug.Log ("OnWebCamTextureToMatHelperErrorOccurred " + errorCode);
+    }
+    
+    void OnDestroy()
+    {
+        _webCamTextureToMatHelper.Dispose ();
+    }
+}

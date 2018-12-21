34bfb5d8d61415d7b43913d93942ef7dfe1da376
Takaya Ogawa <bumpofchicken344@gmail.com>
2018-12-22 00:49:56 +0900

    add ToMatHelperManager

---
+++ Assets/Scripts/ToMatHelperManager.cs
@@ -0,0 +1,68 @@
+﻿using OpenCVForUnityExample;
+using UnityEngine;
+
+public class ToMatHelperManager
+{
+    GameObject _quad;
+    WebCamTextureToMatHelper _texToMatHelper;
+    FpsMonitor _fpsMonitor;
+
+    public ToMatHelperManager(GameObject quad, WebCamTextureToMatHelper texToMatHelper, FpsMonitor fpsMonitor)
+    {
+        _quad = quad;
+        _texToMatHelper = texToMatHelper;
+        _fpsMonitor = fpsMonitor;
+    }
+    
+    public void OnWebCamTextureToMatHelperInitialized(ref Texture2D quadTex)
+    {
+        Debug.Log ("OnWebCamTextureToMatHelperInitialized");
+
+        var webCamTextureMat = _texToMatHelper.GetMat();
+        quadTex = new Texture2D (webCamTextureMat.cols(), webCamTextureMat.rows(), TextureFormat.RGBA32, false);
+        _quad.GetComponent<Renderer>().material.mainTexture = quadTex;
+
+        Debug.Log ("Screen.width " + Screen.width + " Screen.height " + Screen.height + " Screen.orientation " + Screen.orientation);
+
+        if (_fpsMonitor != null){
+            _fpsMonitor.Add ("width", webCamTextureMat.width().ToString());
+            _fpsMonitor.Add ("height", webCamTextureMat.height().ToString());
+            _fpsMonitor.Add ("orientation", Screen.orientation.ToString());
+        }
+        
+        float width = webCamTextureMat.width();
+        float height = webCamTextureMat.height();
+                                
+        float widthScale = Screen.width / width;
+        float heightScale = Screen.height / height;
+        if (widthScale < heightScale) {
+            Camera.main.orthographicSize = (width * Screen.height / Screen.width) / 2;
+        } else {
+            Camera.main.orthographicSize = height / 2;
+        }
+        
+        //Quadを画面いっぱいにリサイズ
+        ////https: //blog.narumium.net/2016/12/11/unityでスマホカメラを全面表示する/
+        var quadHeight = Camera.main.orthographicSize * 2;
+        var quadWidth = quadHeight * Camera.main.aspect;
+        _quad.transform.localScale = new Vector3(quadWidth, quadHeight, 1);
+    }
+
+    public void OnWebCamTextureToMatHelperDisposed(ref Texture2D quadTex)
+    {
+        Debug.Log ("OnWebCamTextureToMatHelperDisposed");
+        if (quadTex != null) {
+            Object.Destroy(quadTex);
+            quadTex = null;
+        }
+    }
+    
+    public void OnWebCamTextureToMatHelperErrorOccurred(WebCamTextureToMatHelper.ErrorCode errorCode){
+        Debug.Log ("OnWebCamTextureToMatHelperErrorOccurred " + errorCode);
+    }
+    
+    void OnDestroy()
+    {
+        _texToMatHelper.Dispose ();
+    }
+}
--- Assets/Scripts/WebCamManager.cs
+++ Assets/Scripts/WebCamManager.cs
@@ -10,17 +10,20 @@ public class WebCamManager : MonoBehaviour
     Texture2D _quadTex;
     WebCamTextureToMatHelper _texToMatHelper;
     FpsMonitor _fpsMonitor;
+    ToMatHelperManager _toMatHelperManager;
     InvisibleConverter _invCvtr;
     float _time = 5;
     
     void Start()
     {
-        _invCvtr = new InvisibleConverter(_text);
         Input.backButtonLeavesApp = true;
         Screen.sleepTimeout = SleepTimeout.NeverSleep;
         
+        _invCvtr = new InvisibleConverter(_text);
+        
         _fpsMonitor = GetComponent<FpsMonitor>();
         _texToMatHelper = GetComponent<WebCamTextureToMatHelper>();
+        _toMatHelperManager = new ToMatHelperManager(gameObject, _texToMatHelper, _fpsMonitor);
         _texToMatHelper.Initialize();
     }
     
@@ -41,56 +44,16 @@ public class WebCamManager : MonoBehaviour
         var webCamMat = _invCvtr.CvtToInvisible(_texToMatHelper.GetMat());
         Utils.fastMatToTexture2D(webCamMat, _quadTex);
     }
-    
-    public void OnWebCamTextureToMatHelperInitialized()
-    {
-        Debug.Log ("OnWebCamTextureToMatHelperInitialized");
 
-        var webCamTextureMat = _texToMatHelper.GetMat();
-        _quadTex = new Texture2D (webCamTextureMat.cols(), webCamTextureMat.rows(), TextureFormat.RGBA32, false);
-        GetComponent<Renderer>().material.mainTexture = _quadTex;
-
-        Debug.Log ("Screen.width " + Screen.width + " Screen.height " + Screen.height + " Screen.orientation " + Screen.orientation);
-
-        if (_fpsMonitor != null){
-            _fpsMonitor.Add ("width", webCamTextureMat.width().ToString());
-            _fpsMonitor.Add ("height", webCamTextureMat.height().ToString());
-            _fpsMonitor.Add ("orientation", Screen.orientation.ToString());
-        }
-        
-        float width = webCamTextureMat.width();
-        float height = webCamTextureMat.height();
-                                
-        float widthScale = Screen.width / width;
-        float heightScale = Screen.height / height;
-        if (widthScale < heightScale) {
-            Camera.main.orthographicSize = (width * Screen.height / Screen.width) / 2;
-        } else {
-            Camera.main.orthographicSize = height / 2;
-        }
-        
-        //Quadを画面いっぱいにリサイズ
-        ////https: //blog.narumium.net/2016/12/11/unityでスマホカメラを全面表示する/
-        var quadHeight = Camera.main.orthographicSize * 2;
-        var quadWidth = quadHeight * Camera.main.aspect;
-        transform.localScale = new Vector3(quadWidth, quadHeight, 1);
+    public void OnWebCamTextureToMatHelperInitialized(){
+        _toMatHelperManager.OnWebCamTextureToMatHelperInitialized(ref _quadTex);
     }
 
-    public void OnWebCamTextureToMatHelperDisposed ()
-    {
-        Debug.Log ("OnWebCamTextureToMatHelperDisposed");
-        if (_quadTex != null) {
-            Destroy(_quadTex);
-            _quadTex = null;
-        }
+    public void OnWebCamTextureToMatHelperDisposed (){
+        _toMatHelperManager.OnWebCamTextureToMatHelperDisposed(ref _quadTex);
     }
     
     public void OnWebCamTextureToMatHelperErrorOccurred(WebCamTextureToMatHelper.ErrorCode errorCode){
-        Debug.Log ("OnWebCamTextureToMatHelperErrorOccurred " + errorCode);
-    }
-    
-    void OnDestroy()
-    {
-        _texToMatHelper.Dispose ();
+        _toMatHelperManager.OnWebCamTextureToMatHelperErrorOccurred(errorCode);
     }
 }
